801c136bb8f48885ef7c98f0f3b4402420c89bb9
VISHNU07202003 <padyalavishnusai@ufl.edu>
2025-12-29 00:23:46 -0500

    certificates.js

---
+++ js/data/certificates.js
@@ -0,0 +1,32 @@
+const certificatesData = [
+    {
+        id: 1,
+        title: "Microsoft Azure Administrator (AZ-104)",
+        image: "images/certificates/azure-az104.jpg"
+    },
+    {
+        id: 2,
+        title: "Google UX Design Foundations",
+        image: "images/certificates/google-ux.jpg"
+    },
+    {
+        id: 3,
+        title: "TensorFlow Developer Certificate",
+        image: "images/certificates/tensorflow.jpg"
+    },
+    {
+        id: 4,
+        title: "Deep Learning Specialization",
+        image: "images/certificates/deep-learning.jpg"
+    },
+    {
+        id: 5,
+        title: "AWS Solutions Architect",
+        image: "images/certificates/aws-solutions.jpg"
+    },
+    {
+        id: 6,
+        title: "Data Structures & Algorithms",
+        image: "images/certificates/dsa.jpg"
+    }
+];
