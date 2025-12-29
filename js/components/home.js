982132542f9f86ba1e02c3e7f58ea0f8f1d02639
VISHNU07202003 <padyalavishnusai@ufl.edu>
2025-12-29 00:24:42 -0500

    home.js

---
+++ js/components/home.js
@@ -0,0 +1,19 @@
+function renderHomePage() {
+    return `
+        <div class="home-container">
+            <h1 class="home-title">VISHNU SAI PADYALA</h1>
+            <p class="home-tagline">
+                A creative mind building clean, functional digital experiences.
+            </p>
+            <canvas id="threeCanvas" width="500" height="400"></canvas>
+            <div class="cta-buttons">
+                <button class="btn btn-primary" onclick="navigateTo('projects')">
+                    View Projects
+                </button>
+                <button class="btn btn-secondary" onclick="navigateTo('contact')">
+                    Get in Touch
+                </button>
+            </div>
+        </div>
+    `;
+}
